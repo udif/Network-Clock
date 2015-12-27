419d0d35876fa2a2400ff3c40899b60079f62896
Udi Finkelstein <github@udifink.com>
2015-12-27 22:36:56 +0200

    Added NTP code as well.

--- ESP8266/GetLocalTime/GetLocalTime.ino
+++ ESP8266/GetLocalTime/GetLocalTime.ino
@@ -9,7 +9,7 @@
 
 #include <ESP8266WiFi.h>
 #include <ESP8266WiFiMulti.h>
-
+#include <WiFiUdp.h>
 #include <ESP8266HTTPClient.h>
 
 #define USE_SERIAL Serial
@@ -17,6 +17,8 @@
 #include "my_ssid.h"
 
 ESP8266WiFiMulti WiFiMulti;
+
+void setup_ntp(void);
 
 static char tzdata[100] = "/lookup?tzid=";
 static uint8_t comma_cnt = 0, tz_cnt = 13; // tzdata length
@@ -161,13 +163,21 @@ void process_http(char *host, char *path, void (*process_result)(uint8_t *buff, 
       USE_SERIAL.print("[HTTP] GET... failed, no connection or no HTTP server\n");
     }
 }
+
+void setup_web(void)
+{
+    USE_SERIAL.print("[HTTP] begin...\n");
+    // configure traged server and url
+    process_http("ip-api.com", "/csv", &process_ip_api);
+    USE_SERIAL.println(tzdata);
+    USE_SERIAL.println();
+
+}
 void setup() {
 
   USE_SERIAL.begin(115200);
   // USE_SERIAL.setDebugOutput(true);
 
-  USE_SERIAL.println();
-  USE_SERIAL.println();
   USE_SERIAL.println();
 
   for (uint8_t t = 4; t > 0; t--) {
@@ -177,7 +187,110 @@ void setup() {
   }
 
   WiFiMulti.addAP(MY_SSID, MY_PASSWORD);
+  setup_web();
+  setup_ntp();
 
+}
+
+//
+//  NTP code
+//
+static unsigned int localPort = 2390;      // local port to listen for UDP packets
+IPAddress timeServerIP; // time.nist.gov NTP server address
+//const char* ntpServerName = "time.nist.gov";
+const char* ntpServerName = "pool.ntp.org";
+const int NTP_PACKET_SIZE = 48; // NTP time stamp is in the first 48 bytes of the message
+byte packetBuffer[ NTP_PACKET_SIZE]; //buffer to hold incoming and outgoing packets
+
+// A UDP instance to let us send and receive packets over UDP
+WiFiUDP udp;
+
+// send an NTP request to the time server at the given address
+unsigned long sendNTPpacket(IPAddress& address)
+{
+  Serial.println("sending NTP packet...");
+  // set all bytes in the buffer to 0
+  memset(packetBuffer, 0, NTP_PACKET_SIZE);
+  // Initialize values needed to form NTP request
+  // (see URL above for details on the packets)
+  packetBuffer[0] = 0b11100011;   // LI, Version, Mode
+  packetBuffer[1] = 0;     // Stratum, or type of clock
+  packetBuffer[2] = 6;     // Polling Interval
+  packetBuffer[3] = 0xEC;  // Peer Clock Precision
+  // 8 bytes of zero for Root Delay & Root Dispersion
+  packetBuffer[12]  = 49;
+  packetBuffer[13]  = 0x4E;
+  packetBuffer[14]  = 49;
+  packetBuffer[15]  = 52;
+
+  // all NTP fields have been given values, now
+  // you can send a packet requesting a timestamp:
+  udp.beginPacket(address, 123); //NTP requests are to port 123
+  udp.write(packetBuffer, NTP_PACKET_SIZE);
+  udp.endPacket();
+}
+
+void setup_ntp(void)
+{
+  udp.begin(localPort);
+}
+
+void get_ntp(void)
+{
+    //get a random server from the pool
+  WiFi.hostByName(ntpServerName, timeServerIP); 
+
+  sendNTPpacket(timeServerIP); // send an NTP packet to a time server
+  // wait to see if a reply is available
+  delay(1000);
+  
+  int cb = udp.parsePacket();
+  if (!cb) {
+    Serial.println("no packet yet");
+  }
+  else {
+    Serial.print("packet received, length=");
+    Serial.println(cb);
+    // We've received a packet, read the data from it
+    udp.read(packetBuffer, NTP_PACKET_SIZE); // read the packet into the buffer
+
+    //the timestamp starts at byte 40 of the received packet and is four bytes,
+    // or two words, long. First, esxtract the two words:
+
+    unsigned long highWord = word(packetBuffer[40], packetBuffer[41]);
+    unsigned long lowWord = word(packetBuffer[42], packetBuffer[43]);
+    // combine the four bytes (two words) into a long integer
+    // this is NTP time (seconds since Jan 1 1900):
+    unsigned long secsSince1900 = highWord << 16 | lowWord;
+    Serial.print("Seconds since Jan 1 1900 = " );
+    Serial.println(secsSince1900);
+
+    // now convert NTP time into everyday time:
+    Serial.print("Unix time = ");
+    // Unix time starts on Jan 1 1970. In seconds, that's 2208988800:
+    const unsigned long seventyYears = 2208988800UL;
+    // subtract seventy years:
+    unsigned long epoch = secsSince1900 - seventyYears;
+    // print Unix time:
+    Serial.println(epoch);
+
+
+    // print the hour, minute and second:
+    Serial.print("The UTC time is ");       // UTC is the time at Greenwich Meridian (GMT)
+    Serial.print((epoch  % 86400L) / 3600); // print the hour (86400 equals secs per day)
+    Serial.print(':');
+    if ( ((epoch % 3600) / 60) < 10 ) {
+      // In the first 10 minutes of each hour, we'll want a leading '0'
+      Serial.print('0');
+    }
+    Serial.print((epoch  % 3600) / 60); // print the minute (3600 equals secs per minute)
+    Serial.print(':');
+    if ( (epoch % 60) < 10 ) {
+      // In the first 10 seconds of each minute, we'll want a leading '0'
+      Serial.print('0');
+    }
+    Serial.println(epoch % 60); // print the second
+  }
 }
 
 void get_time()
@@ -187,16 +300,12 @@ void get_time()
 
     HTTPClient http;
 
-    USE_SERIAL.print("[HTTP] begin...\n");
-    // configure traged server and url
-    process_http("ip-api.com", "/csv", &process_ip_api);
-    USE_SERIAL.println(tzdata);
-    USE_SERIAL.println();
     datetime_index = 0;
     tzdata_state = 255;
     process_http("www.tzdata.org", &tzdata[0], &process_tzdata_org_json);
     USE_SERIAL.println(datetime);
     USE_SERIAL.println();
+    get_ntp();
   }
 }
 
